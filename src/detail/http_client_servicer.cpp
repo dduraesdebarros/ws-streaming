1916c91edfc9c5dcb09673d2fe649ec1293899bf
Daniel Barros <daniel.duraesdebarros@hbkworld.com>
2026-08-11 12:00:34 +0000

    debug logs

--- src/detail/http_client_servicer.cpp
+++ src/detail/http_client_servicer.cpp
@@ -2,6 +2,7 @@
 #include <chrono>
 #include <cstddef>
 #include <functional>
+#include <iostream>
 #include <string>
 #include <utility>
 
@@ -83,6 +84,17 @@ void wss::detail::http_client_servicer::finish_read(
     if (ec)
         return close(ec);
 
+    auto log_line = [](const std::string& message)
+    {
+        std::clog << "[DanielDebug] " << message << '\n';
+    };
+
+    log_line(
+        std::string("http rx method=")
+        + std::string(req.method_string())
+        + " target=" + std::string(req.target())
+        + " body_size=" + std::to_string(req.body().size()));
+
     auto get_header = [&](boost::beast::http::field field)
     {
         auto it = std::find_if(
@@ -100,6 +112,8 @@ void wss::detail::http_client_servicer::finish_read(
 
     if (get_header(boost::beast::http::field::upgrade) == "websocket" && !key.empty())
     {
+        log_line("http path=websocket-upgrade");
+
         auto response_key = detail::websocket_protocol::get_response_key(key);
 
         boost::beast::http::response<boost::beast::http::string_body> res(
@@ -122,6 +136,8 @@ void wss::detail::http_client_servicer::finish_read(
 
     else if (req.method() == boost::beast::http::verb::post)
     {
+        log_line("http path=command-post");
+
         nlohmann::json request_json;
 
         try
@@ -144,6 +160,14 @@ void wss::detail::http_client_servicer::finish_read(
                 req,
                 boost::beast::http::status::bad_request,
                 { { "code", -32700 }, { "message", "Request object is invalid" } });
+
+        log_line(
+            std::string("jsonrpc method=")
+            + request_json["method"].get<std::string>()
+            + " params_type="
+            + (request_json.contains("params")
+                ? request_json["params"].type_name()
+                : "null"));
 
         boost::optional<nlohmann::json> response_json;
 
@@ -170,6 +194,11 @@ void wss::detail::http_client_servicer::finish_read(
                 boost::beast::http::status::internal_server_error,
                 { { "code", -32700 }, { "message", "No connected slot" } });
 
+        log_line(
+            std::string("command result type=")
+            + response_json.value().type_name()
+            + " body=" + response_json.value().dump());
+
 
         return do_response(
             req,
@@ -179,6 +208,8 @@ void wss::detail::http_client_servicer::finish_read(
 
     else if (req.method() == boost::beast::http::verb::options)
     {
+        log_line("http path=options");
+
         return do_response(
             req,
             boost::beast::http::status::no_content,
@@ -248,9 +279,20 @@ void wss::detail::http_client_servicer::do_response(
     res.set(boost::beast::http::field::access_control_allow_headers, "*");
     res.set(boost::beast::http::field::access_control_allow_origin, "*");
 
+    auto log_line = [](const std::string& message)
+    {
+        std::clog << "[DanielDebug] " << message << '\n';
+    };
+
     if (!response_json.is_null())
     {
         bool is_success;
+
+        log_line(
+            std::string("compat check status=")
+            + std::to_string(static_cast<unsigned>(status))
+            + " json_type=" + response_json.type_name()
+            + " json_body=" + response_json.dump());
 
         if (response_json.is_array())
         {
@@ -279,14 +321,25 @@ void wss::detail::http_client_servicer::do_response(
         {
             res.set(boost::beast::http::field::content_type, "text/plain");
             res.body() = "Succeeded";
+            log_line("compat branch=Succeeded");
         }
 
         else
         {
             res.set(boost::beast::http::field::content_type, "application/json");
             res.body() = response_json.dump();
+            log_line("compat branch=application/json");
         }
     }
+
+    log_line(
+        std::string("tx pre-prepare status=")
+        + std::to_string(static_cast<unsigned>(status))
+        + " content_type="
+        + (res.find(boost::beast::http::field::content_type) != res.end()
+            ? std::string(res.find(boost::beast::http::field::content_type)->value())
+            : "<unset>")
+        + " body=" + res.body());
 
     do_response(res);
 }
